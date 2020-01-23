535b0c509e4cff49baeee7fb7f83d5d4265ef518
Evan Pezent <epezent@rice.edu>
2020-01-22 22:59:16 -0600

    add coroutine file

---
+++ examples/coroutines.cpp
@@ -0,0 +1,67 @@
+#include <mahi/gui.hpp>
+
+using namespace mahi::gui;
+
+class CoroDemo : public Application {
+public:
+    CoroDemo() : Application(250,250, "Coroutine Demo", false) { 
+        backgroundColor = Grays::Black;
+    }
+
+    void update() {
+        // controls
+        ImGui::SetNextWindowPos({10,10});
+        ImGui::Begin("Coro Demo", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+        if (ImGui::Button("Move X")) 
+            startCoroutine(move(0));
+        ImGui::SameLine();
+        if (ImGui::Button("Move Y"))
+            startCoroutine(move(1));
+        ImGui::SameLine();
+        if (ImGui::Button("Blink"))
+            startCoroutine(blink());
+        ImGui::SameLine();
+        if (ImGui::Button("Go Crazy"))
+            startCoroutine(goCrazy());
+        ImGui::Text("Coroutine Count: %d", coroutineCount());
+        ImGui::End();
+        // draw
+        ImGui::GetBackgroundDrawList()->AddCircleFilled(pos, 50, col, 100);
+    }
+
+    Enumerator move(int axis) {
+        float t = 0;
+        while (t < 5) {
+            pos[axis] = 125 + 50 * std::sin(2*Math::PI*t + Math::HALFPI * axis);
+            t += ImGui::GetIO().DeltaTime;
+            co_yield nullptr;
+        }
+    }
+
+    Enumerator blink() {
+        auto col1 = ImGui::GetColorU32(Grays::Black);
+        auto col2 = ImGui::GetColorU32(Greens::Chartreuse);
+        for (int i = 0; i < 20; ++i) {
+            col = col1;
+            co_yield new WaitForSeconds(0.1f);
+            col = col2;
+            co_yield new WaitForSeconds(0.15f);
+        }
+    }
+
+    Enumerator goCrazy() {
+        startCoroutine(blink());
+        startCoroutine(move(0));
+        co_yield startCoroutine(move(1));
+    }
+
+    ImVec2 pos = {125,175};
+    ImU32 col = ImGui::GetColorU32(Greens::Chartreuse);
+};
+
+int main(int argc, char const *argv[])
+{
+    CoroDemo demo;
+    demo.run();
+    return 0;
+}
