44e7a274233964e2d9b7570d3e22d4e48d2cbc5b
Evan Pezent <epezent@rice.edu>
2020-01-26 01:00:22 -0600

    add imgui as repo

--- src/mahi/Application.cpp
+++ src/mahi/Application.cpp
@@ -1,8 +1,8 @@
 #include <mahi/Application.hpp>
 
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "examples/imgui_impl_glfw.h"
+#include "examples/imgui_impl_opengl3.h"
 #include <stdio.h>
 #include <iostream>
 #include <stdexcept>
