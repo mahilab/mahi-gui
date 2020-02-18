ac35f4c643b683f01a3eb7c8947d3c3d2f0bcffc
epezent <epezent@rice.edu>
2020-02-17 23:05:49 -0600

    clean up rt_plots

--- examples/rt_plots.cpp
+++ examples/rt_plots.cpp
@@ -9,38 +9,30 @@ public:
         ImGui::DisableViewports();
         items1.resize(1);
         plot1.xAxis.maximum = 10;
-        plot1.xAxis.lockMin = true;
-        plot1.xAxis.lockMax = true;
+        plot1.xAxis.lockMin = true;  plot1.xAxis.lockMax = true;
         items2 = items1;
         plot2 = plot1;
     }
 
     void update() override {
         ImGui::BeginFixed("Real Time Plots", {0,0}, {800,500}, ImGuiWindowFlags_NoTitleBar);     
-
         // rolling plot example
         ImGui::PlotItemRollPoint(items1[0], (float)time(), Random::range(0.49f,0.51f), 10.0f);
         ImGui::Plot("My Plot", plot1, items1, ImVec2(-1,240));
-
         // buffering plot example
         ImGui::PlotItemBufferPoint(items2[0], (float)time(), Random::range(0.49f,0.51f), 1000);
         plot2.xAxis.minimum = (float)time() - 10;
         plot2.xAxis.maximum = (float)time();
         ImGui::Plot("My Plot", plot2, items2, ImVec2(-1,-1));
-        
         ImGui::End();
     }
 
-
-    int mode = 0; // 0 rolling , 1 buffering
     ImGui::PlotInterface plot1, plot2;
     std::vector<ImGui::PlotItem> items1, items2;
 };
 
-
 int main(int argc, char const *argv[])
 {
-
     RealTimePlotDemo demo;
     demo.run();
     return 0;
