b3b7bda8b696457b6d6e23b1e94d0be0ee1668c2
Evan Pezent <epezent@rice.edu>
2020-04-01 13:54:57 -0500

    update ex_shapes

--- examples/ex_shapes.cpp
+++ examples/ex_shapes.cpp
@@ -7,10 +7,16 @@ using namespace mahi::util;
 class ShapeDemo : public Application {
 public:
     ShapeDemo(const Config& cfg) : Application(cfg) { 
+
         shape = make_polygon_shape(5, 50);
         shape.set_radii(10);
-        shape.move(100,100);
         shape2 = offset_shape(shape, 20);
+        star = make_star_shape(5, 15, 30);
+        star.set_radii(2);
+
+        hole_shape = make_polygon_shape(6, 50);
+        hole_shape.set_hole_count(1);
+        hole_shape.set_hole(0, make_star_shape(6, 30, 20));
 
         auto s1 = make_rectangle_shape(50,50);
         s1.move(250,50);
@@ -28,21 +34,22 @@ public:
 
         s1.move(100,0);
         s2.move(100,0);
-        ints_shape = clip_shapes(s1,s2,ClipType::Intersection);
-
-        star = make_star_shape(5, 15, 30);
-        star.move(100,100);
-        star.set_radii(2);
-
-        hole_shape = make_polygon_shape(6, 50);
-        hole_shape.set_hole_count(1);
-        hole_shape.set_hole(0, make_star_shape(6, 30, 20));
-        
+        ints_shape = clip_shapes(s1,s2,ClipType::Intersection);        
     }
+    
     void draw(NVGcontext* nvg) override {  
+
+        nvgTranslate(nvg, 100, 100);
         nvgFillShape(nvg, shape2, Greens::ForestGreen);
         nvgFillShape(nvg, shape,  Greens::Chartreuse);
         nvgFillShape(nvg, star,   Greens::ForestGreen);
+        nvgResetTransform(nvg);
+
+        nvgTranslate(nvg, 100, 400);
+        nvgFillShape(nvg, hole_shape, Blues::DeepSkyBlue);
+        nvgStrokeShape(nvg, hole_shape, 5, Whites::Azure);
+        nvgResetTransform(nvg);
+
         for (auto& s : union_shape)
             nvgFillShape(nvg, s, Reds::FireBrick);
         for (auto& s : diff_shape)
@@ -51,21 +58,17 @@ public:
             nvgFillShape(nvg, s, Reds::FireBrick);
         for (auto& s : ints_shape)
             nvgFillShape(nvg, s, Reds::FireBrick);
-
-        nvgTranslate(nvg, 100, 400);
-        nvgFillShape(nvg, hole_shape, Blues::DeepSkyBlue);
-        nvgStrokeShape(nvg, hole_shape, 5, Whites::Azure);
-        nvgResetTransform(nvg);
     } 
 
     Shape shape;
     Shape shape2;
+    Shape star;
+    Shape hole_shape;
+
     std::vector<Shape> union_shape;
     std::vector<Shape> diff_shape;
     std::vector<Shape> excl_shape;
     std::vector<Shape> ints_shape;
-    Shape hole_shape;
-    Shape star;
 };
 
 int main(int argc, char const *argv[])
